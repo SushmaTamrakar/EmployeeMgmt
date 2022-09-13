dcdce2cfe6f82ee02b35d0a420fc54a291e3f668
SushmaTamrakar <113170396+SushmaTamrakar@users.noreply.github.com>
2022-09-13 19:55:37 +0545

    Add files via upload

---
+++ EmployeeMgmtSystem/applyleave.php
@@ -0,0 +1,154 @@
+<?php 
+	$id = (isset($_GET['id']) ? $_GET['id'] : '');
+	require_once ('process/dbh.php');
+	$sql = "SELECT * FROM `employee` where id = '$id'";
+	$result = mysqli_query($conn, $sql);
+	$employee = mysqli_fetch_array($result);
+	$empName = ($employee['firstName']);
+	//echo "$id";
+?>
+
+<html>
+<head>
+	<title>Apply Leave | Employee Panel | Employee Management System</title>
+	<link rel="stylesheet" type="text/css" href="styleapply.css">
+</head>
+<body bgcolor="#F0FFFF">
+	
+	<header>
+		<nav>
+			<h1>Employee Management System</h1>
+			<ul id="navli">
+				<li><a class="homeblack" href="eloginwel.php?id=<?php echo $id?>"">HOME</a></li>
+				<li><a class="homeblack" href="myprofile.php?id=<?php echo $id?>"">My Profile</a></li>
+				<li><a class="homeblack" href="empproject.php?id=<?php echo $id?>"">My Projects</a></li>
+				<li><a class="homered" href="applyleave.php?id=<?php echo $id?>"">Apply Leave</a></li>
+				<li><a class="homeblack" href="elogin.html">Log Out</a></li>
+			</ul>
+		</nav>
+	</header>
+	 
+	<div class="divider"></div>
+	<div class="page-wrapper bg-blue p-t-100 p-b-100 font-robo">
+        <div class="wrapper wrapper--w680">
+            <div class="card card-1">
+                <div class="card-heading"></div>
+                <div class="card-body">
+                    <h2 class="title">Apply Leave Form</h2>
+                    <form action=  "process/applyleaveprocess.php?id=<?php echo $id?>" id="leaveForm" method="POST" onsubmit=" return myFunction()">
+
+
+					<div class="input-group">
+                            
+							<div class="col-4">
+                                <div class="input-group">
+                                    <div class="rs-select2 js-select-simple select--no-search">
+                                        <select name="reason">
+                                            <option disabled="disabled" selected="selected">REASON</option>
+                                            <option value="Causal Leave">Causal Leave</option>
+                                            <option value="Annual Leave">Annual Leave</option>
+                                            <option value="Festival Leave">Festival Leave</option>
+											<option value="Sick Leave">Sick Leave</option>
+											<option value="Maternity Leave">Maternity Leave</option>
+											
+                                        </select>
+                                        <div class="select-dropdown"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row row-space">
+                            <div class="col-2">
+                            	<p>Start Date</p>
+                                <div class="input-group">
+                                    <input  id ="leaveStartDate"class="input--style-1" type="date" placeholder="Start Date" name="start" required>
+                                   
+                                </div>
+                            </div>
+                            <div class="col-2">
+                            	<p>End Date</p>
+                                <div class="input-group">
+                                    <input id="leaveEndDate" class="input--style-1" type="date" placeholder="End Date" name="end" required>
+                                   
+                                </div>
+                            </div>
+                        </div>
+                        
+
+
+
+                        <div class="p-t-20">
+                            <button class="btn btn--radius btn--green" type="submit">Submit</button>
+				
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+	<script>
+		function myFunction() {
+var startdate = document.getElementById("leaveStartDate").value;
+var enddate = document.getElementById("leaveEndDate").value;
+  var today	 = new Date();
+
+  if (new Date(startdate).getTime() < today	.getTime() || new Date(enddate).getTime() < today	.getTime()) {
+    alert("Date is invalid ");
+    return false;
+  }
+  return true;
+}
+
+</script>
+
+
+	<table>
+			<tr>
+				<th align = "center">Emp. ID</th>
+				<th align = "center">Name</th>
+				<th align = "center">Start Date</th>
+				<th align = "center">End Date</th>
+				<th align = "center">Total Days</th>
+				<th align = "center">Reason</th>
+				<th align = "center">Status</th>
+			</tr>
+
+
+			<?php
+
+
+				$sql = "Select employee.id, employee.firstName, employee.lastName, employee_leave.start, employee_leave.end, employee_leave.reason, employee_leave.status From employee, employee_leave Where employee.id = $id and employee_leave.id = $id order by employee_leave.token";
+				$result = mysqli_query($conn, $sql);
+				while ($employee = mysqli_fetch_assoc($result)) {
+					$date1 = new DateTime($employee['start']);
+					$date2 = new DateTime($employee['end']);
+					$interval = $date1->diff($date2);
+					$interval = $date1->diff($date2);
+
+					echo "<tr>";
+					echo "<td>".$employee['id']."</td>";
+					echo "<td>".$employee['firstName']." ".$employee['lastName']."</td>";
+					
+					echo "<td>".$employee['start']."</td>";
+					echo "<td>".$employee['end']."</td>";
+					echo "<td>".$interval->days."</td>";
+					echo "<td>".$employee['reason']."</td>";
+					echo "<td>".$employee['status']."</td>";
+					
+				}
+
+
+			?>
+
+
+		</table>
+
+
+
+
+
+
+
+</body>
+</html>
